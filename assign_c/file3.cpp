39440f8c24c66baaeef6c6df0c106f7351441abc
VikasPatnala <vikaspatnala2003@gmail.com>
2024-01-13 12:23:35 +0530

    Updated with overloading

--- assign_c/file3.cpp
+++ assign_c/file3.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
-bool isPalindrome(string &str)
+int add(int a, int b)
 {
-    string reversedStr = str;
-    reverse(reversedStr.begin(), reversedStr.end());
+    return a + b;
+}
 
-    return str == reversedStr;
+string add(const string &str1, const string &str2)
+{
+    return str1 + str2;
+}
+
+double add(double x, double y, double z)
+{
+    return x + y + z;
 }
 
 int main()
 {
-    string input;
+    int result1 = add(5, 3);
+    cout << "Result of adding integers: " << result1 << endl;
 
-    cout << "Enter a string: ";
-    cin >> input;
+    string result2 = add("Hello, ", "world!");
+    cout << "Result of concatenating strings: " << result2 << endl;
 
-    if (isPalindrome(input)) {
-        cout << "The entered string is a palindrome.\n";
-    }
-    else {
-        cout << "The entered string is not a palindrome.\n";
-    }
+    double result3 = add(2.5, 3.5, 1.0);
+    cout << "Result of adding doubles: " << result3 << endl;
 
     return 0;
 }

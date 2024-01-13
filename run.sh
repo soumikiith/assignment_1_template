fd21b21a9783e1c1e2281e9c84a7cf0e026dd4f9
Umesh-k26 <umeshkalvakuntla26@gmail.com>
2024-01-13 12:23:02 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-export LLVM_INSTALL_DIR=$1
-
+LLVM_BUILD_DIR=$1
 if [[ -z "$1" ]]; then
-    echo "Please provide LLVM_INSTALL_DIR"
+    echo "Please provide LLVM_BUILD_DIR as 1st argument"
     echo "Exiting..."
     exit 0
 fi
-
-OPT=$LLVM_INSTALL_DIR/bin/opt
-CLANG=$LLVM_INSTALL_DIR/bin/clang
+OPT=$LLVM_BUILD_DIR/bin/opt
+CLANG=$LLVM_BUILD_DIR/bin/clang
 OUT_DIR=./assign_C/output_IR
 INP_DIR=./assign_C
-
+mkdir -p $OUT_DIR
 
 $CLANG $INP_DIR/file1.c -S -emit-llvm -o $OUT_DIR/file1.ll
+
+# write the similar commands for remaining files to generate .ll files
